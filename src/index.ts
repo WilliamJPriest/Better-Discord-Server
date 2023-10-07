3bfc1a71c1750c00a2d4c073e907e346b7d3a46e
williamjpriest <liuliu6wp@gmail.com>
2023-10-07 05:55:57 -0400

    socket worksish

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,15 @@ import { Server } from "socket.io";
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+const io = new Server(httpServer, {  cors: {
+  origin: "*",//temp
+}})
 
 console.log("hello")
-io.on("connection", (socket: { id: any; }) => {
-  console.log(socket.id)
+io.on("connection", (socket) => {
+  socket.emit("hello", "yo");
 });
 
-httpServer.listen(3000);
+httpServer.listen(4000, function() {
+  console.log(`Listening on port 4000`);
+});
