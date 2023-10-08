dcecf731dda607bcbea7e609d3f335df0188037a
williamjpriest <liuliu6wp@gmail.com>
2023-10-08 03:54:06 -0400

    server host restricted

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,18 @@ import { Server } from "socket.io";
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, {  cors: {
-  origin: "*",//temp
+const io = new Server(httpServer, {    cors: {
+  origin: "http://localhost:5173",
+  methods: ["GET", "POST"]
 }})
 
 console.log("hello")
-io.on("connection", (socket) => {
-  socket.emit("hello", "yo");
+io.on("connection", socket => {  
+  socket.on("message",args =>{
+    socket.broadcast.emit("recmessage", args);
+  })
 });
+
 
 httpServer.listen(4000, function() {
   console.log(`Listening on port 4000`);
