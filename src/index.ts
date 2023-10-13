76aa258c83a024a694035793667bcbc3691c5cb3
williamjpriest <liuliu6wp@gmail.com>
2023-10-13 04:27:03 -0400

    server returns usernames

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,27 @@ import { Server } from "socket.io";
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {    cors: {
-  origin: "http://localhost:5173",
+  origin: "http://localhost:5174",
   methods: ["GET", "POST"]
 }})
 
-console.log("hello")
-io.on("connection", socket => {  
+// type OnlineUsersType= {
+//   name: string;
+// }
+let onlineUsers: {name: string; }[] = [];
+io.on("connection", socket => {     
+  console.log(socket.id)
+
+  socket.on("online", user =>{
+    onlineUsers.push({name: user})
+    console.log(onlineUsers)
+    socket.emit("onlineU",onlineUsers)
+  })
   socket.on("message",args =>{
+    console.log(args)
     socket.broadcast.emit("recmessage", args);
   })
-  let allUsers: { id: string; username: any; room: any; }[] = [];
-  socket.on("someroom", args =>{
-    socket.join(args.room)
-    let room = args.room;
-    let username = args.name
-    allUsers.push({ id: socket.id, username, room });
-    let chatRoomUsers = allUsers.filter((user) => user.room === room);
-    socket.to(room).emit('chatroom_users', chatRoomUsers);
-    socket.emit('chatroom_users', chatRoomUsers);
-  })
+
 });
 
 
