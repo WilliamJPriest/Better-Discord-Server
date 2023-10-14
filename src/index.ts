44288bf201c39b39588bfe911c0be6d9e2e3109b
williamjpriest <liuliu6wp@gmail.com>
2023-10-14 13:50:37 -0400

    messy server update

--- src/index.ts
+++ src/index.ts
@@ -6,34 +6,62 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {    cors: {
   origin: "http://localhost:5173",
-  methods: ["GET", "POST"]
-}})
+  methods: ["GET", "POST"],
+  
+}
 
-// type OnlineUsersType= {
-//   name: string;
-// }
-let onlineUsers: {name: string; }[] = [];
-io.on("connection", socket => {     
-  console.log(socket.id)
+})
+declare module 'socket.io' {
+  interface Socket {
+    username?: string;
+  }
+}
 
-  socket.on("online", user =>{
-    // onlineUsers.push({"name": user})
-    // console.log(onlineUsers)
-    // user = onlineUsers
-    console.log(user)
-    socket.emit("onlineU",user)
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error("invalid username"));
+  }
+  socket.username = username;
+  next();
+});
+
+io.on("connection", (socket) => {
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({
+      userID: id,
+      username: socket.username,
+    });
+    console.log("Hello")
+  }
+  socket.emit("users", users);
+
+  socket.broadcast.emit("user connected", {
+    userID: socket.id,
+    username: socket.username,
   })
+  // console.log("hi")
+});
+
+//   socket.on("online", (user) =>{
+//     // onlineUsers.push(user)
+//     // console.log(onlineUsers)
+//     // user = onlineUsers
+//     console.log(user)
+//     socket.emit("onlineU",(user))
+//   })
 
   
-  socket.on("message",(args, tacos) =>{
+//   socket.on("message",(args, tacos) =>{
    
-    args.socket_id = socket.id 
-    console.log(args)
-    tacos ="tacos"
-    socket.emit("recmessage", (args),(tacos));
-  })
+//     args.socket_id = socket.id 
+//     console.log(args)
+//     tacos ="tacos"
+//     socket.broadcast.emit("recmessage", (args),(tacos));
+//     // socket.emit("onlineU",(args)) works on send
+//   })
 
-});
 
 
 httpServer.listen(4000, function() {
